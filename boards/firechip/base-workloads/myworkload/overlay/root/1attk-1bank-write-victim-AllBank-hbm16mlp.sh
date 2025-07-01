27c0fc878c9760f5cd8b376901f75ae8e97ee2b2
amin-mamandi <amin.mamandi@ku.edu>
2025-07-01 08:45:41 -0500

    bank-pll for gem5 exps + new m5ops

---
+++ boards/firechip/base-workloads/myworkload/overlay/root/1attk-1bank-write-victim-AllBank-hbm16mlp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Start attacker in background with large iteration count
+./bankpll -m 40960 -a write -b 0x78000 -l 16 -i 50000000 -c 0 -e 0 -x -s 1 -n 1 -A -P 0 -C 0 &
+ATTACKER_PID=$!
+
+# Run victim with small iteration count
+./bankpll -m 40960 -a read -b 0x78000 -l 16 -i 50 -c 3 -x -s 0 -n 1
+
+# When victim finishes, terminate attacker and get its bandwidth
+echo "Victim finished, terminating attacker..."
+kill -TERM $ATTACKER_PID
+
+# Wait for attacker to print its final bandwidth report
+wait $ATTACKER_PID
+echo "All processes completed"
+m5 exit
